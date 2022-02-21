39781e40b7f5ba8c093f289b14be5cf48c6fc539
Peter Javorsky <tekk.sk@gmail.com>
2022-02-21 14:33:26 +0100

    Added platfrom CubeCell

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,13 @@
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
-void setup() {
+void setup() 
+{
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
 }
 
-void loop() {
+void loop() 
+{
   delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
   Serial.print("Ping: ");
   Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
