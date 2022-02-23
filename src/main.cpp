c22a3d4b96a567bd3ef2272572ff70ffac9ae676
Peter Javorsky <tekk.sk@gmail.com>
2022-02-23 03:41:07 +0100

    Working on old ESP8266 with JSN-SR04T

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,87 @@
 #include <Arduino.h>
+
+// Include DHT Libraries from Adafruit
+// Dependant upon Adafruit_Sensors Library
+#include <DHT.h>
+
+// Include NewPing Library
 #include <NewPing.h>
 
-#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+// Define Constants
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+#define DHTPIN 7       // DHT-22 Output Pin connection
+#define DHTTYPE DHT22   // DHT Type is DHT 22 (AM2302)
+#define TRIGGER_PIN  4
+#define ECHO_PIN     5
+#define MAX_DISTANCE 400
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+// Define Variables
+
+float hum;    // Stores humidity value in percent
+float temp;   // Stores temperature value in Celcius
+float duration; // Stores HC-SR04 pulse duration value
+float distance; // Stores calculated distance in cm
+float soundsp;  // Stores calculated speed of sound in M/S
+float soundcm;  // Stores calculated speed of sound in cm/ms
+int iterations = 5;
+
+// Initialize DHT sensor for normal 16mhz Arduino
+
+DHT dht(DHTPIN, DHTTYPE); 
 
 void setup() 
 {
-  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+  Serial.begin (115200);
+  //dht.begin();
 }
 
-void loop() 
+void loop()
 {
-  delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-  Serial.print("Ping: ");
-  Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
-  Serial.println("cm");
+  delay(2000);  // Delay so DHT-22 sensor can stabalize
+   
+  //hum = dht.readHumidity();  // Get Humidity value
+  //temp = dht.readTemperature();  // Get Temperature value
+
+  hum = 45;
+  temp = 21;
+
+  // Calculate the Speed of Sound in M/S
+  soundsp = 331.4 + (0.606 * temp) + (0.0124 * hum);
+
+  // Convert to cm/ms
+  soundcm = soundsp / 10000;
+
+  duration = sonar.ping_median(iterations);
+
+  // Calculate the distance
+  distance = (duration / 2) * soundcm;
+
+  // Send results to Serial Monitor
+  Serial.print("Sound: ");
+  Serial.print(soundsp);
+  Serial.print(" m/s, ");
+  Serial.print("Humid: ");
+  Serial.print(hum);
+  Serial.print(" %, Temp: ");
+  Serial.print(temp);
+  Serial.print(" C, ");
+  Serial.print("Distance: ");
+
+  // Serial.println(sonar.ping_cm());
+  // delay(200);
+
+  if (distance >= 400 || distance <= 2)
+  {
+    Serial.print("Out of range");
+  }
+  else
+  {
+    Serial.print(distance);
+    Serial.print(" cm");
+    delay(500);
+  }
+
+  Serial.println(" ");
 }
